23e79f457b85434244117d1533991ea1a87a971e
Çağatay Kalan <ckalan@gmail.com>
2026-02-06 02:29:49 +0300

    Register checked exceptions cache as session component

--- common/src/main/kotlin/com/bivektor/kotlin/fir/CheckedExceptionsFirExtensionRegistrar.kt
+++ common/src/main/kotlin/com/bivektor/kotlin/fir/CheckedExceptionsFirExtensionRegistrar.kt
@@ -1,13 +1,11 @@
 package com.bivektor.kotlin.fir
 
-import org.jetbrains.kotlin.fir.analysis.extensions.FirAdditionalCheckersExtension
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 
 class CheckedExceptionsFirExtensionRegistrar : FirExtensionRegistrar() {
     override fun ExtensionRegistrarContext.configurePlugin() {
         registerDiagnosticContainers(CheckedExceptionsErrors)
-        +FirAdditionalCheckersExtension.Factory { session ->
-            CheckedExceptionsAdditionalCheckersExtension(session)
-        }
+        +::CheckedExceptionsCache
+        +::CheckedExceptionsAdditionalCheckersExtension
     }
 }
